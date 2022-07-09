82a33b91b15dc5be64b86663a680a67a9213ffa2
hykare <78385964+hykare@users.noreply.github.com>
2022-07-09 22:02:19 +0200

    end game when guess is correct

--- hangman.rb
+++ hangman.rb
@@ -1,11 +1,12 @@
 class Hangman
   attr_reader :secret_word
-  attr_accessor :word, :guesses_left
+  attr_accessor :word, :guesses_left, :message
 
   def initialize
     @secret_word = pick_word
     @word = '_' * secret_word.length
     @guesses_left = 15
+    @message = ''
   end
 
   def pick_word
@@ -18,6 +19,11 @@ class Hangman
     puts word.split('').join(' ')
   end
 
+  def play
+    play_round until over?
+    show_result
+  end
+
   def play_round
     puts "guesses left: #{guesses_left}"
     print_word
@@ -25,16 +31,26 @@ class Hangman
     # TODO: validate input, disable enter
     guess = gets.chomp.downcase
 
+    # update word
     if @secret_word.include? guess
       secret_word.each_char.with_index do |char, i|
         word[i] = guess if char == guess
       end
     end
-
     self.guesses_left -= 1
+    # check for game end
+  end
+
+  def over?
+    self.message = "Congratulations! You won! The word was \"#{secret_word}\"" if word == secret_word
+    self.message = "You failed. The word was \"#{secret_word}\"" if guesses_left.zero?
+    word == secret_word || guesses_left.zero?
+  end
+
+  def show_result
+    puts message
   end
 end
 
 game = Hangman.new
-game.play_round while game.guesses_left.positive?
-puts "the word was #{game.secret_word}"
+game.play
